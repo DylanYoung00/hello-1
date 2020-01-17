f4425a2a656e8f260e09eaf8029be5db2c9fc756
DylanYoung00 <59969031+DylanYoung00@users.noreply.github.com>
2020-01-16 21:57:47 -0600

    added  ;

---
+++ hello2.c
@@ -0,0 +1,20 @@
+/*
+Dylan Young
+January 16, 2020
+Source Code for Hack 1
+*/
+
+
+
+
+#include <stdio.h>
+
+int main(void) {
+	
+	printf("Dylan Young\n");
+	printf("Computer Science\n");
+	printf("Sam Pane\n");
+	printf("Computer Science\n");
+	
+    return 0;
+}
